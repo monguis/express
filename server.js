d86f82941f279ad02cb1fba0789d2c3d5d8fb452
monguis <jccnnetwork@gmail.com>
2020-01-23 15:29:36 -0700

    saved few files to prevent issues

--- server.js
+++ server.js
@@ -58,13 +58,27 @@ app.get("/", function (req, res) {
 // //   * POST `/api/notes` - Should recieve a new note to save on the request body, 
 // //   add it to the `db.json` file, and then return the new note to the client.
 
+
+// * DELETE `/api/notes/:id` - Should recieve a query paramter containing the id of a note to delete. This means you
+// 'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, 
+// you'll need to read all notes from the 
+// `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+app.delete("/api/notes/:id", (req,res)=>{
+    id = req.params.reservations;
+    dbJSON = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+    auxElement = dbJSON[id];
+    dbJSON.splice(id,1);
+    fs.writeFileSync('db/db.json', JSON.stringify(dbJSON,null,2,"utf-8"))
+    res.json(auxElement);
+})
+
+
 app.post(`/api/notes`, (req, res) => {
     const notetoAdd = req.body;
     dbJSON = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
     console.log(dbJSON)
     dbJSON.push(notetoAdd);
     fs.writeFileSync('db/db.json', JSON.stringify(dbJSON,null,2,"utf-8"))
-    
     res.json(notetoAdd);
   });
 
